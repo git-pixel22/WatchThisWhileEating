5e3f6f7b82d8e3947c1d4711daeb030cccb7ac29
git-pixel22 <workemail.rajat@gmail.com>
2024-04-21 12:07:10 +0530

    adds files

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
